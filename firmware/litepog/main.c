de7477e34eaaccbdc3adfe82cd3ee6ea264950bd
Jasper Chan <jasperchan515@gmail.com>
2021-11-30 12:31:07 -0800

    firmware: finish idle animation logic, implement winner logic

--- firmware/litepog/main.c
+++ firmware/litepog/main.c
@@ -1,8 +1,10 @@
 #include <msp430.h> 
+#include <stdbool.h>
+#include <stdint.h>
 #include "ws2812.h"
 #include "config.h"
 
-volatile long long counter;
+unsigned volatile long long counter;
 
 
 // Set DCO == SMCLK == MCLK == 24 MHz
@@ -72,8 +74,38 @@ void initTimer(void) {
     TB1CTL|= MC__UP;
 }
 
-void demoLoop() {
+void drawCountDown(uint8_t state) {
+    const uint8_t middle_idx = NPX_NUM_LEDS/2;
+    const uint8_t max_left = middle_idx - (COUNTDOWN_NUMBER - 1)*PIXEL_WIDTH;
+    const uint8_t max_right = middle_idx + (COUNTDOWN_NUMBER - 1)*PIXEL_WIDTH + (PIXEL_WIDTH - 1);
+    uint8_t i, j;
+    uint8_t pos_left, pos_right;
+
+    clearStrip();
+    if(state < COUNTDOWN_NUMBER) {
+        // Show countdown going up (red lights)
+        for(i=0; i<=state; i++) {
+            pos_left = middle_idx - i*PIXEL_WIDTH;
+            pos_right = middle_idx + i*PIXEL_WIDTH;
+            for(j=0; j<PIXEL_WIDTH; j++) {
+                setLEDColor(pos_left + j, 255, 0, 0);
+                setLEDColor(pos_right + j, 255, 0, 0);
+            }
+        }
+    } else {
+        // Show countdown finishing (green light)
+        for(i=max_left; i<=max_right; i++)
+            setLEDColor(i, 0, 255, 0);
+    }
+    showStrip();
+}
+
+void demoLoop(void) {
     int npx_idx = 0;
+    bool countdown_started = false;
+    unsigned long long countdown_start;
+    unsigned long long demo_counter = 0;
+    uint8_t countdown_state;
     while(1) {
         if(BTN_PxIN & BTN1_PIN)
             LED_PxOUT &= ~(LED1_PIN);
@@ -85,21 +117,84 @@ void demoLoop() {
         else
             LED_PxOUT |= LED2_PIN;
 
-        // Update Neopixel strip about every 1 s
-        if(counter > 1000) {
-            counter = 0;
-            clearStrip();
-            setLEDColor(npx_idx, 0, 0, 255);
+        if(!(BTN_PxIN & (BTN1_PIN | BTN2_PIN))) {
+            if(!countdown_started) {
+                countdown_started = true;
+                countdown_start = counter;
+            }
+            if(
+                    counter >=
+                    countdown_start + countdown_state*COUNTDOWN_INTERVAL) {
+                drawCountDown(countdown_state++);
+
+                if(countdown_state > COUNTDOWN_NUMBER) {
+                    LED_PxOUT &= ~(LED1_PIN | LED2_PIN);
+                    return;
+                }
+            }
+
+        } else {
+            // Reset countdown, show demo loop
+            countdown_started = false;
+            countdown_state = 0;
+            if(counter > demo_counter + DEMO_INTERVAL) {
+                demo_counter = counter;
+                clearStrip();
+                setLEDColor(npx_idx, 0, 0, 255);
+                showStrip();
+                if(++npx_idx >= NPX_NUM_LEDS)
+                    npx_idx = 0;
+            }
+        }
+    }
+}
+
+uint8_t gameLoop(void) {
+    // TODO: Implement game logic
+    while(1) {
+        if(BTN_PxIN & BTN1_PIN)
+            return 0;
+
+        if(BTN_PxIN & BTN2_PIN)
+            return 1;
+    }
+}
+
+void winnerLoop(uint8_t winner) {
+    uint8_t i = 0;
+    uint8_t j;
+    uint8_t winner_idx, loser_idx, idx1, idx2;
+    unsigned long long anim_start = counter;
+
+    clearStrip();
+    while(i<WINNER_NUMBER*2) {
+        if (counter >= anim_start + i*WINNER_INTERVAL) {
+            if(i%2) {
+                clearStrip();
+            } else {
+                for(j=0; j < WINNER_LED_WIDTH; j++) {
+                    idx1 = j;
+                    idx2 = NPX_NUM_LEDS - 1 - j;
+                    if(winner) {
+                        loser_idx = idx1;
+                        winner_idx = idx2;
+                    } else {
+                        loser_idx = idx2;
+                        winner_idx = idx1;
+                    }
+                    setLEDColor(winner_idx, 0, 255, 0);
+                    setLEDColor(loser_idx, 255, 0, 0);
+                }
+            }
             showStrip();
-            if(++npx_idx >= NPX_NUM_LEDS)
-                npx_idx = 0;
+            i++;
         }
     }
 }
 
 int main(void)
 {
-    volatile long i;
+    uint8_t winner;
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
 	initGpio();
@@ -112,7 +207,11 @@ int main(void)
 
 	__enable_interrupt();
 
-	demoLoop();
+	while(1) {
+        demoLoop();
+        winner = gameLoop();
+        winnerLoop(winner);
+	}
 
 	return 0;
 }
